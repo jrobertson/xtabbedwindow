1e61610808d4f03decab88c85a6b233051e4e575
James Robertson <james@jamesrobertson.eu>
2018-09-05 12:34:53 +0100

    feature: Within public method *goto_tab* if the tab does not exist then an attempt is made to load a new tab with the location given.

--- lib/xtabbedwindow.rb
+++ lib/xtabbedwindow.rb
@@ -14,9 +14,9 @@ class XTabbedWindow
   # next_tab values e.g. Ctrl+Tab (default), Ctrl+Alt+Page_Up
   #
   def initialize(name, new_tab: "Ctrl+t", scan_tabs: true, 
-                 next_tab: "Ctrl+Tab")
+                 next_tab: "Ctrl+Tab", location_key: 'Ctrl+l')
 
-    @new_tab, @next_tab = new_tab, next_tab
+    @new_tab, @next_tab, @location_key = new_tab, next_tab, location_key
     a = WMCtrl.instance.windows
     @window = a.detect {|x| x.title =~ /#{name}/i}
 
@@ -45,18 +45,29 @@ class XTabbedWindow
       r = a.grep(regex)
     end
 
-    return if r.empty?
+    if r.empty? then
+            
+      new_tab()
+      
+      # open the document
+      XDo::Keyboard.char(@location_key)
+      
+      block_given? ? yield : return
+      
+    else
 
-    i = a.index(r.first)
+      i = a.index(r.first)
 
-    @window.activate
-    sleep 0.1
+      @window.activate
+      sleep 0.1
 
-    XDo::Keyboard.char("Alt+1")
-    sleep 0.1
-    i.times { XDo::Keyboard.char(@next_tab) }
+      XDo::Keyboard.char("Alt+1")
+      sleep 0.1
+      i.times { XDo::Keyboard.char(@next_tab) }
+      
+      true
     
-    true
+    end
 
   end
   
